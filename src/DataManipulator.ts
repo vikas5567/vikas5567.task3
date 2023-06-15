e45c509ee4937472a7916a8efbe0ca2f01445ad8
Ismarji Workman <ismarjiw@protonmail.com>
2023-06-15 15:45:23 -0400

    update typos

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -15,9 +15,9 @@ export class DataManipulator {
   static generateRow(serverRespond: ServerRespond[]): Row {
     const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2; 
     const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2; 
-    const ratio = priceABC + priceDEF;
+    const ratio = priceABC / priceDEF;
     const upperBound = 1 + 0.05;
-    const lowerBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
 
       return {
         price_abc: priceABC,
