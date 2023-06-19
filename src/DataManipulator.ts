eb40cfaf4262c2cc05083758e355c132b6407b82
Armish11 <135872279+Armish11@users.noreply.github.com>
2023-06-20 02:06:36 +0500

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,36 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  ratio: number;
+  lower_bound: number;
+  upper_bound: number;
+  trigger_alert?: number;
+  price_abc: number;
+  price_def: number;
+  timestamp: Date;
 }
 
-
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1.05;
+    const lowerBound = 0.95;
+    let triggerAlert;
+
+    if (ratio > upperBound || ratio < lowerBound) {
+      triggerAlert = ratio;
+    }
+
+    return {
+      ratio,
+      lower_bound: lowerBound,
+      upper_bound: upperBound,
+      trigger_alert: triggerAlert,
+      price_abc: priceABC,
+      price_def: priceDEF,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+    };
   }
 }
