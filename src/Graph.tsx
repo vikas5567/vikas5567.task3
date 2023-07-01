91a5e451534b158989c4462446b3bcce9d14b9d7
Steve Calla <callasteven@gmail.com>
2023-07-01 16:02:13 -0600

    Adjust format of function

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -73,7 +73,7 @@ class Graph extends Component<IProps, {}> {
 //   componentDidUpdate() {
 //     if (this.table) {
 //       this.table.update([
-//         DataManipulator.generateRow(this.props.data),
+//              DataManipulator.generateRow(this.props.data),
 //         ] as unknown as TableData
 //       );
 //     }
