d0cd727ce3e1aa79237b4faceadfff121c10c77d
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-20 17:20:33 -0500

    component lifecycle method

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -55,9 +55,9 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
