8a3406160d1a7d1c103d564320e40c0d3cec4d6a
adarsh800 <Adarsh_shukla@outlook.com>
2024-03-18 20:42:10 +0530

    Update the contentupdate

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -55,9 +55,8 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([DataManipulator.generateRow(this.props.data),
+      ]as unknown as TableData);
     }
   }
 }
