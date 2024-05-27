582cb827b8b150e4436583e652e81dbd0f09049d
alexlostorto <lostorto.business@gmail.com>
2024-05-27 01:26:24 +0100

    My lower bound was the same as my upper bound :0

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -12,12 +12,12 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverRespond: ServerRespond[]) {
+  static generateRow(serverRespond: ServerRespond[]): Row {
     const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2
     const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2
     const ratio = priceABC / priceDEF
     const upperBound = 1 + 0.10
-    const lowerBound = 1 + 0.10
+    const lowerBound = 1 - 0.10
     return {
       price_abc: priceABC,
       price_def: priceDEF,
