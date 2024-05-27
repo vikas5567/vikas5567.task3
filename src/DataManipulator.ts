60102e7b2fb10df9bd18355b8b13c0806975d462
alexlostorto <lostorto.business@gmail.com>
2024-05-27 01:00:37 +0100

    Fix DataManipulator class

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -12,22 +12,20 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2
-    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2
+  static generateRow(serverRespond: ServerRespond[]) {
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2
     const ratio = priceABC / priceDEF
     const upperBound = 1 + 0.10
     const lowerBound = 1 + 0.10
-    return serverResponds.map((el: any) => {
-      return {
-        price_abc: priceABC,
-        price_def: priceDEF,
-        ratio,
-        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
-        upper_bound: upperBound,
-        lower_bound: lowerBound,
-        trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
-      };
-    })
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
 }
